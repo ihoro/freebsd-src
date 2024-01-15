d5d21c7610abb89aaef2dc2f95a0a6d75b12173c
Igor Ostapenko <pm@igoro.pro>
2024-01-15 22:09:16 +0200

    kyua: migrate process::jail::exec from jexec(8) to jail syscalls + lib

--- contrib/kyua/utils/process/jail.cpp
+++ contrib/kyua/utils/process/jail.cpp
@@ -30,8 +30,15 @@
 extern "C" {
 #include <unistd.h>
 #include <sys/stat.h>
+
+// FreeBSD Jail syscalls
+#include <sys/param.h>
+#include <sys/jail.h>
+#define JAIL_MAX 999999
+
+// FreeBSD Jail library
+#include <jail.h>
 }
-#define JAIL_MAX 999999 // <sys/jail.h>
 
 #include <fstream>
 #include <iostream>
@@ -44,7 +51,6 @@ extern "C" {
 
 namespace fs = utils::fs;
 namespace process = utils::process;
-namespace jail = utils::process::jail;
 
 using utils::process::args_vector;
 using utils::process::child;
@@ -143,7 +149,8 @@ public:
 /// \param jail_name Name of a new jail.
 /// \param jail_params String of jail parameters.
 void
-jail::create(const std::string& jail_name, const std::string& jail_params)
+process::jail::create(const std::string& jail_name,
+                      const std::string& jail_params)
 {
     args_vector av;
 
@@ -151,10 +158,11 @@ jail::create(const std::string& jail_name, const std::string& jail_params)
     av.push_back("-qc");
 
     // jail name
-    av.push_back(F("name=%s") % jail_name);
+    av.push_back("name=" + jail_name);
 
     // some obvious defaults to ease test authors' life
-    av.push_back(F("children.max=%d") % JAIL_MAX);
+    // jail's children.max param must be lower than parent jail's max value,
+    av.push_back("children.max=" + std::to_string(JAIL_MAX - 1));
 
     // test defined jail params
     const std::vector< std::string > params = parse_jail_params_string(jail_params);
@@ -187,14 +195,11 @@ jail::create(const std::string& jail_name, const std::string& jail_params)
 /// \param program The test program binary absolute path.
 /// \param args The arguments to pass to the binary, without the program name.
 void
-jail::exec(const std::string& jail_name,
-           const fs::path& program,
-           const args_vector& args) throw()
+process::jail::exec(const std::string& jail_name,
+                    const fs::path& program,
+                    const args_vector& args) throw()
 {
-    args_vector av(args);
-    av.insert(av.begin(), program.str());
-
-    // get our work dir
+    // get work dir prepared by kyua
     char cwd[256];
     if (getcwd(cwd, 256) == NULL) {
         std::cerr << "process::jail::exec: getcwd() errors: "
@@ -202,32 +207,29 @@ jail::exec(const std::string& jail_name,
         std::exit(EXIT_FAILURE);
     }
 
-    // prepare a script to run in a jail to change back to the work dir
-    // and exec the program
-    std::string cd_exec_path = std::string(cwd) + "/kyua_cd_exec.sh";
-    std::ofstream f(cd_exec_path);
-    if (f.fail()) {
-        std::cerr << "process::jail::exec: cannot create kyua_cd_exec.sh file: "
-            << strerror(errno) << ".\n";
+    // get jail id by its name
+    int jid = jail_getid(jail_name.c_str());
+    if (jid < 0) {
+        std::cerr << "process::jail::exec: jail_getid() errors: "
+            << strerror(errno) << ": " << jail_errmsg << ".\n";
         std::exit(EXIT_FAILURE);
     }
-    f << "#!/bin/sh\n"
-      << "cd \"$1\" && shift && exec $*";
-    f.close();
-    if (chmod(cd_exec_path.c_str(),
-              S_IRUSR|S_IXUSR | S_IRGRP|S_IXGRP | S_IROTH|S_IXOTH) != 0) {
-        std::cerr << "process::jail::exec: chmod() errors: "
+
+    // attach to the jail
+    if (jail_attach(jid) == -1) {
+        std::cerr << "process::jail::exec: jail_attach() errors: "
             << strerror(errno) << ".\n";
         std::exit(EXIT_FAILURE);
     }
 
-    // change current work dir inside a jail back to kyua work dir
-    av.insert(av.begin(), cwd);
-    av.insert(av.begin(), cd_exec_path);
+    // set back the expected work dir
+    if (chdir(cwd) == -1) {
+        std::cerr << "process::jail::exec: chdir() errors: "
+            << strerror(errno) << ".\n";
+        std::exit(EXIT_FAILURE);
+    }
 
-    av.insert(av.begin(), jail_name);
-
-    process::exec(fs::path("/usr/sbin/jexec"), av);
+    process::exec(program, args);
 }
 
 
@@ -237,7 +239,7 @@ jail::exec(const std::string& jail_name,
 ///
 /// \param jail_name Name of a jail to remove.
 void
-jail::remove(const std::string& jail_name)
+process::jail::remove(const std::string& jail_name)
 {
     args_vector av;
 
