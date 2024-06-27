6a8153ccd2914b3d58f68f3fe063f7074351adfe
Igor Ostapenko <pm@igoro.pro>
2024-06-27 19:24:49 +0300

    if_enc: pullup if m_len == 0

--- sys/net/if_enc.c
+++ sys/net/if_enc.c
@@ -289,6 +289,8 @@ enc_hhook(int32_t hhook_type, int32_t hhook_id, void *udata, void *ctx_data,
 #ifdef INET
 	case AF_INET:
 		ph = V_inet_pfil_head;
+		if ((*ctx->mp)->m_len == 0)
+			*ctx->mp = m_pullup(*ctx->mp, sizeof(struct ip));
 		break;
 #endif
 #ifdef INET6
