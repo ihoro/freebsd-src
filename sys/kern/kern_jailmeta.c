55c9b89dcca7ca19d7a106d310dcc1744d3001f1
Igor Ostapenko <igoro@FreeBSD.org>
2024-11-17 13:56:43 +0000

    Drop osd create method

--- sys/kern/kern_jailmeta.c
+++ sys/kern/kern_jailmeta.c
@@ -49,44 +49,6 @@ static u_int jm_osd_slot;
 
 /* OSD_JAIL methods */
 
-/* TODO: it looks we should do nothing on create phase, anyway set is called
- * right after create.
- * The only useful thing could be to pre-allocate osd slot, probably.
- * But be ready for a case if a jail was created before the module loaded. */
-//static int
-//jm_osd_method_create(void *obj, void *data)
-//{
-//	struct prison *pr = obj;
-//	struct vfsoptlist *opts = data;
-//	char *opt_addr;
-//	int opt_len; /* TODO: is it provided w/o NULL char? */
-//	char *osd_addr;
-//	void **rsv;
-//	int error;
-//
-//	error = vfs_getopt(opts, "meta", (void **)&opt_addr, &opt_len);
-//	if (error != 0)
-//		return (0);
-//	/* printf("_create: opt_meta=%s, len=%d | ", opt_meta, len); */
-//
-//	/* TODO: check for JAILMETA_BUF_MAXLEN */
-//
-//	osd_addr = malloc(opt_len + 1, M_PRISON, M_WAITOK);
-//	memcpy(osd_addr, opt_addr, opt_len);
-//	osd_addr[opt_len] = '\0';
-//
-//	rsv = osd_reserve(jm_osd_slot);
-//	/* TODO: what if rsv is NULL? */
-//	mtx_lock(&pr->pr_mtx);
-//	(void) osd_jail_set_reserved(pr, jm_osd_slot, rsv, osd_addr);
-//	/* printf("_create: osd_meta=%s | ", osd_meta); */
-//	mtx_unlock(&pr->pr_mtx);
-//
-//	/* TODO: remember about osd_free_reserved() */
-//
-//	return (0);
-//}
-
 static int
 jm_osd_method_set(void *obj, void *data)
 {
@@ -183,7 +145,6 @@ static int
 jm_sysinit(void *arg __unused)
 {
 	osd_method_t methods[PR_MAXMETHOD] = {
-		//[PR_METHOD_CREATE] = jm_osd_method_create, // TODO: it looks we could remove this
 		[PR_METHOD_SET] = jm_osd_method_set,
 		[PR_METHOD_GET] = jm_osd_method_get,
 		[PR_METHOD_CHECK] = jm_osd_method_check,
